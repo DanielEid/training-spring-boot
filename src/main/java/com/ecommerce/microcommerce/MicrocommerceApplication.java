a044fe7c9c54dd960ddf184cfa75994bc2cef21c
Daniel Eid <danieleid06@gmail.com>
2020-01-06 14:34:43 +0100

    6- remove tests issues

--- src/main/java/com/ecommerce/microcommerce/MicrocommerceApplication.java
+++ src/main/java/com/ecommerce/microcommerce/MicrocommerceApplication.java
@@ -2,10 +2,13 @@ package com.ecommerce.microcommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableWebSecurity
+
 public class MicrocommerceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/ecommerce/microcommerce/configuration/WebSecurityConfig.java
+++ src/main/java/com/ecommerce/microcommerce/configuration/WebSecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,7 +13,6 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import java.util.Arrays;
 
 
-@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
